374180385cdab9c949a880386b0e00d1c729102c
adiletmaratov <adilet.maratov@gmail.com>
2017-01-17 13:37:14 +0600

    добавил класс MachineTrainer и его инициализацию

---
+++ machine_trainer.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+
+
+class MachineTrainer(object):
+    def __init__(self):
+        self._classifier = self._construct_classifier()
+        self.is_trained = False
+        self._data_set = None
+
+    @staticmethod
+    def _construct_classifier():
+        return Pipeline([
+            ('vect', CountVectorizer()),
+            ('tfidf', TfidfTransformer()),
+            ('clf',
+             SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5,
+                           random_state=42)),
+        ])
