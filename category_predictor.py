731920321c839bf9f4879ec675163e75b10d973e
adiletmaratov <adilet.maratov@gmail.com>
2017-01-17 13:41:30 +0600

    добавил метод train_machine для предварительного обучения машины

--- category_predictor.py
+++ category_predictor.py
@@ -5,3 +5,5 @@ class CategoryPredictor(object):
     def __init__(self):
         self._machine_trainer = MachineTrainer()
 
+    def train_machine(self):
+        self._machine_trainer.train_machine()
