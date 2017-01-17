65dbe5816b02b9195aa3dd9f6f12694dd5e3ae0b
adiletmaratov <adilet.maratov@gmail.com>
2017-01-17 13:42:14 +0600

    добавил методы predict_category и print_result для определения категории и вывода результатов

--- category_predictor.py
+++ category_predictor.py
@@ -7,3 +7,14 @@ class CategoryPredictor(object):
 
     def train_machine(self):
         self._machine_trainer.train_machine()
+
+    def predict_category(self, user_input):
+        user_input = [user_input]
+        predicted = self._machine_trainer.classifier.predict(user_input)
+        self._print_results(user_input, predicted)
+
+    def _print_results(self, user_input, predicted):
+        for new_sentence, category in zip(user_input, predicted):
+            result = "{} => {}".format(new_sentence,
+                                       self._machine_trainer.data_set.target_names[category])
+            print(result)
