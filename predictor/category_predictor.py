2af8d3453bd09fe9986e0330c7c4f59c27beb82e
adiletmaratov <adilet.maratov@gmail.com>
2017-01-17 19:30:32 +0600

    добавил docstrings и удалил ненужный метод

--- predictor/category_predictor.py
+++ predictor/category_predictor.py
@@ -1,16 +1,14 @@
-from .machine_trainer import MachineTrainer
-
-
+# coding=utf-8
 class CategoryPredictor(object):
-    def __init__(self):
-        self._machine_trainer = MachineTrainer()
-
-    def train_machine(self):
-        self._machine_trainer.train()
+    """Класс определяет категорию предложения после обучения данными из
+    http://qwone.com/~jason/20Newsgroups/ """
+    def __init__(self, machine_trainer):
+        self._machine_trainer = machine_trainer
+        self._classifier = machine_trainer.classifier
 
     def predict(self, user_input):
         user_input = [user_input]
-        predicted = self._machine_trainer.classifier.predict(user_input)
+        predicted = self._classifier.predict(user_input)
         self._print_results(user_input, predicted)
 
     def _print_results(self, user_input, predicted):
